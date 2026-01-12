fc854d2998a30de69f487f544d0b5c7a7f904c6e
Anvarjon <anvarjondevelop@gmail.com>
2026-01-12 10:42:27 +0900

    BRR: Intial Setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("EXECUTED");
+
+const person: string = "Martin";
+const count: number = 100;
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY-MM-DD");
+console.log(currentTime);
