0e61964e6ee77324c6841494d0225a7d2207bcc9
Anvarjon <anvarjondevelop@gmail.com>
2026-01-15 08:42:02 +0900

    feat: added Task H

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,14 @@
+//TASK H
 
+// Shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib,
+// faqat positive qiymatlarni olib string holatda return qilsin.
+//  MASALAN: getPositive([1, -4, 2]) return qiladi "12".
+
+function getIntData(a: number[]): string {
+  const filtered = a.filter((n) => n > 0);
+  return filtered.join(" ");
+}
+const data = getIntData([1, 4, 3, 4, 5, -1, -3, 1]);
+console.log(data);
+
+//------------------------------------------------------------------------
