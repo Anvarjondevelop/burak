4aec90431f4c84b3a024a582cdbf5e5fb1050075
Anvarjon <anvarjondevelop@gmail.com>
2026-01-17 15:25:17 +0900

    feat: develop member schema model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
+// Schema first & code first
+const memeberSchema = new Schema(
+  {
+    memberType: {
+      type: String,
+      enum: MemberType,
+      default: MemberType.USER,
+    },
+    memberStatus: {
+      type: String,
+      enum: MemberStatus,
+      default: MemberStatus.ACTIVE,
+    },
+    memberNick: {
+      type: String,
+      index: { unique: true, sparse: true }, //faqat mavjud (bor) qiymatlar indexga kiradinull yoki yo‘q bo‘lgan field indexlanmaydi
+      required: true,
+    },
+    memberPhone: {
+      type: String,
+      index: { unique: true, sparse: true },
+      required: true,
+    },
+    memberPassword: {
+      type: String,
+      select: false,
+      required: true,
+    },
+    memberAddress: {
+      type: String,
+    },
+    memberDesc: {
+      type: String,
+    },
+    memberImage: {
+      type: String,
+    },
+    memberPoints: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } //updatedAt , createdAt
+);
+export default mongoose.model("Member", memeberSchema);
