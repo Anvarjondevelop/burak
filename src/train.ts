4554883b1fcf02cdbad40376194d62b3f877f4f3
Anvarjon <anvarjondevelop@gmail.com>
2026-01-18 16:40:07 +0900

    fix: fixed Task I

--- src/train.ts
+++ src/train.ts
@@ -3,17 +3,17 @@
 // takrorlangan raqamni topib qaytarsin.
 // MASALAN: majorityElement([1,2,3,4,5,4,3,4]) return 4.
 
-function majorityElement(arr) {
-  const count = {};
+function majorityElement(arr: number[]): number | null {
+  const count: Record<number, number> = {};
 
-  for (let num of arr) {
+  for (const num of arr) {
     count[num] = (count[num] || 0) + 1;
   }
 
-  let maxCount = 0;
-  let result = null;
+  let maxCount: number = 0;
+  let result: number | null = null;
 
-  for (let num in count) {
+  for (const num in count) {
     if (count[num] > maxCount) {
       maxCount = count[num];
       result = Number(num);
@@ -24,7 +24,8 @@ function majorityElement(arr) {
 }
 
 // Test
-majorityElement([1, 2, 3, 4, 5, 4, 3, 4]); // 4
+const result = majorityElement([1, 2, 3, 4, 5, 4, 3, 5]);
+console.log(result);
 
 //TASK H
 
@@ -32,11 +33,11 @@ majorityElement([1, 2, 3, 4, 5, 4, 3, 4]); // 4
 // faqat positive qiymatlarni olib string holatda return qilsin.
 //  MASALAN: getPositive([1, -4, 2]) return qiladi "12".
 
-function getIntData(a: number[]): string {
-  const filtered = a.filter((n) => n > 0);
-  return filtered.join(" ");
-}
-const data = getIntData([1, 4, 3, 4, 5, -1, -3, 1]);
-console.log(data);
+// function getIntData(a: number[]): string {
+//   const filtered = a.filter((n) => n > 0);
+//   return filtered.join(" ");
+// }
+// const data = getIntData([1, 4, 3, 4, 5, -1, -3, 1]);
+// console.log(data);
 
 //------------------------------------------------------------------------
