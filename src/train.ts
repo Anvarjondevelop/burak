05820daeb2341be7c3f98ac8b70564f150329945
Anvarjon <anvarjondevelop@gmail.com>
2026-02-03 20:47:39 +0900

    feat: Solved Task O

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,22 @@
+//O-TASK
+
+//Shunday function yozing, u har xil valuelardan iborat array qabul qilsin va array ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin. MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]) return 45.
+
+function calculateSumOfNumbers(arr) {
+  let sum = 0;
+
+  for (const item of arr) {
+    if (typeof item === "number") {
+      sum += item;
+    }
+  }
+
+  return sum;
+}
+
+calculateSumOfNumbers([10, "10", { son: 10 }, true, 35]);
+// natija: 45
+
 // N-TASK
 
 // Shunday function yozing, u string qabul qilsin va string palindrom yani
@@ -5,13 +24,13 @@
 // aniqlab boolean qiymat qaytarsin.
 // MASALAN: palindromCheck("dad") return true; palindromCheck("son") return false.
 
-function palindromCheck(str: string): boolean {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
-}
+// function palindromCheck(str: string): boolean {
+//   const reversed = str.split("").reverse().join("");
+//   return str === reversed;
+// }
 
-console.log(palindromCheck("Bob"));
-console.log(palindromCheck("Adam"));
+// console.log(palindromCheck("Bob"));
+// console.log(palindromCheck("Adam"));
 
 // M-TASK
 
