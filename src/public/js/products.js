ce6f9c5a6fd668d068e5857a4c6726c212487bfb
Anvarjon <anvarjondevelop@gmail.com>
2026-02-10 20:06:58 +0900

    feat: frontend chosen user update logic develop

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,5 +1,3 @@
-const { cacheSignal } = require("react");
-
 console.log("Products frontend javascript file");
 
 $(function () {
--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,31 @@
 console.log("Users frontend javascript file");
+//document-ready
+$(function () {
+  $(".member-status").on("change", function (e) {
+    const id = e.target.id;
+    console.log("ID", id);
+
+    const memberStatus = $(`#${id}.member-status`).val();
+    console.log("memberStatus:", memberStatus);
+
+    axios
+      .post("/admin/user/edit", { _id: id, memberStatus: memberStatus })
+      .then((response) => {
+        console.log("response:", response);
+        const result = response.data;
+        console.log("result:", result);
+
+        if (result.data) {
+          console.log("result.data", result.data);
+          console.log("User Updated");
+          $(".member-status").blur();
+        } else {
+          alert("User update failed");
+        }
+      })
+      .catch((err) => {
+        console.log("ERROR:", err);
+        alert("User update failed");
+      });
+  });
+});
