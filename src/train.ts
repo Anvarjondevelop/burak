1e137f8123b854eff405b3b5a83dfb8b46fccb60
Anvarjon <anvarjondevelop@gmail.com>
2026-02-17 20:48:20 +0900

    feat : Solved Task U

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,16 @@
+// U-TASK
+
+// Shunday function yozing, uni number parametri bolsin va 0 dan
+// berilgan parametrgacha bolgan oraliqdagi faqat toq sonlar nechtaligini
+//  return qilsin. MASALAN: sumOdds(9) return 4; sumOdds(11) return 5.
+
+function sumOdds(n: number): number {
+  return Math.floor(n / 2);
+}
+
+console.log(sumOdds(9)); // 4
+console.log(sumOdds(11)); // 5
+
 // T-TASK
 
 // Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi
@@ -8,18 +21,17 @@ function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
   return [...arr1, ...arr2].sort((a, b) => a - b);
 }
 
-// testlar
 const result1 = mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]);
-console.log(result1); // [0, 3, 4, 4, 6, 30, 31]
+console.log(result1);
 
 const result2 = mergeSortedArrays([1, 2, 5], [0, 3, 7]);
-console.log(result2); // [0, 1, 2, 3, 5, 7]
+console.log(result2);
 
 const result3 = mergeSortedArrays([], [2, 4, 6]);
-console.log(result3); // [2, 4, 6]
+console.log(result3);
 
 const result4 = mergeSortedArrays([1, 2, 3], []);
-console.log(result4); // [1, 2, 3]
+console.log(result4);
 
 // S-TASK
 
