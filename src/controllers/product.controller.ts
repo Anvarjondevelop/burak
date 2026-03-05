5356d97da8812b00ae4800301f64534054616fa7
Anvarjon <anvarjondevelop@gmail.com>
2026-03-05 11:56:06 +0900

    feat : develop getProduct rest Api

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,7 +3,7 @@ import Errors, { HttpCode, Message } from "../libs/types/Errors";
 import { Request, Response } from "express";
 
 import ProductService from "../models/Product.service";
-import { AdminRequest } from "../libs/types/member";
+import { AdminRequest, ExtendedRequest } from "../libs/types/member";
 import { ProductInput, ProductInquiry } from "../libs/types/product";
 
 const productService = new ProductService();
@@ -37,6 +37,22 @@ productController.getProducts = async (req: Request, res: Response) => {
 // console.log(" req query", query);
 // const params = req.params;
 // console.log("req params:", params);
+
+productController.getProduct = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("getProduct");
+    const { id } = req.params; // destructuring | req.params ichidan id degan property ni olib, id degan o‘zgaruvchiga saqla
+    const memberId = req.member._id ?? null;
+
+    const result = await productService.getProduct(memberId, id as string);
+
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error,  getProduct :", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
 
 /* SSR */
 
